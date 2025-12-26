42d85644942781b30cc2b261d841fc4b4af70de7
Neharika Mainali <neharikamainali9@gmail.com>
2025-12-26 09:30:02 +0545

    trrr

---
+++ week08workshop/student.java
@@ -0,0 +1,12 @@
+package week08workshop;
+
+
+/**
+ * Write a description of class student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class student
+{
+}
---
+++ week08workshop/studenttest.java
@@ -0,0 +1,12 @@
+package week08workshop;
+
+
+/**
+ * Write a description of class studenttest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class studenttest
+{
+}
